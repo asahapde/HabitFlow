2fac3e6bd33f4e61ec43c03d97f58fed24a4e9cd
asahapde <60831459+asahapde@users.noreply.github.com>
2025-03-14 02:56:49 -0400

    fix cors

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -7,6 +7,14 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+// Enable CORS
+app.use(
+  cors({
+    origin: ["http://localhost:3000"], // Allow both local & deployed frontend
+    credentials: true,
+  })
+);
+
 // Middleware
 app.use(cors());
 app.use(express.json());
--- frontend/habitflow/src/pages/Home.tsx
+++ frontend/habitflow/src/pages/Home.tsx
@@ -1,9 +1,26 @@
-import { FC } from "react";
+import axios from "axios";
+import { FC, useEffect, useState } from "react";
 
 const Home: FC = () => {
+  const [data, setData] = useState<string>("Loading...");
+  console.log("API URL:", process.env.REACT_APP_API_URL);
+
+  useEffect(() => {
+    axios
+      .get(`${process.env.REACT_APP_API_URL}`)
+      .then((response) => {
+        setData(response.data);
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+        setData("Failed to load data");
+      });
+  }, []);
+
   return (
     <div>
       <h1>Home</h1>
+      <p>Data from backend: {data}</p>
     </div>
   );
 };
