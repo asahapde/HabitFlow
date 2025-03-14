02e2a00b84065f944ee54b5655940ba6b566f0f7
asahapde <60831459+asahapde@users.noreply.github.com>
2025-03-14 01:59:25 -0400

    Added backend

---
+++ backend/src/server.ts
@@ -0,0 +1,22 @@
+import cors from "cors";
+import dotenv from "dotenv";
+import express from "express";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Routes
+app.get("/", (req, res) => {
+  res.send("Backend is running 🚀");
+});
+
+// Start server
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
